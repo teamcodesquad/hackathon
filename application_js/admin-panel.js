019a00f12e1c1eb78e28c7e936ab69afc852a25c
Avinash Gangadhare <teamcodesquad@gmail.com>
2017-11-08 21:17:40 +0530

    -config changes

--- application_js/admin-panel.js
+++ application_js/admin-panel.js
@@ -1,5 +1,18 @@
 ﻿angular.module('admin-panel', [])
 
+.config(function($sceDelegateProvider) {
+    $sceDelegateProvider.resourceUrlWhitelist([
+      // Allow same origin resource loads.
+      'self',
+      // Allow loading from our assets domain.  Notice the difference between * and **.
+      'http://cdn.rawgit.com/teamcodesquad/hackathon/**'
+    ]);
+
+    // The blacklist overrides the whitelist so the open redirect here is blocked.
+    $sceDelegateProvider.resourceUrlBlacklist([
+      
+    ]);
+})
 .directive('panel',function(){
     return {
         restrict:'E',
